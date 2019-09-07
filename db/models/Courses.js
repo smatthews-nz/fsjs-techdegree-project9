290b0adc811600de52fc8284dc792c4106edd947
Sam Matthews <sam@subsupps.co.nz>
2019-09-07 17:50:32 +1200

    Adds the courses model

---
+++ db/models/Courses.js
@@ -0,0 +1,41 @@
+// Require sequelize
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Course extends Sequelize.Model{}
+
+    Course.init({
+
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        userId: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
+
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+
+        description: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+        },
+
+        estimatedTime: {
+            type: Sequelize.STRING,
+            allowNull: true,
+        },
+
+        materialsNeeded: {
+            type: Sequelize.STRING,
+            allowNull: true,
+        },
+
+    });
+};
